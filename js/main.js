b79d7dab6d5f72a35d5a5e73df0910e3e1e51806
CyberHana <khursandbekov9500@gmail.com>
2025-02-03 16:27:26 +0500

    game-project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+    if (document.body.className != "dark") {
+        this.firstElementChild.src = "images/light.svg";
+    } else {
+        this.firstElementChild.src = "images/dark.svg";
+    }
+    document.body.classList.toggle("dark");
+});
+
+
+
+
+
